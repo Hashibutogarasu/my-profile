0d9e244539365c6fb5e40fd61ca81a855f6d5b16
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2023-10-15 17:38:54 +0900

    new file:   .vscode/launch.json modified:   LICENSE modified:   package.json new file:   public/icons/Karasu.png new file:   public/icons/Minecraft_Launcher_MS_Icon.png new file:   public/icons/genshin_impact.png deleted:    public/icons/nonick.png new file:   public/icons/starrail.png new file:   public/programLangs/java-icon.svg modified:   src/app/page.tsx modified:   src/config/site.ts
    Changes not staged for commit:
    	modified:   README.md
    	deleted:    public/icons/nonickjs.png
    	deleted:    public/icons/nonickstats.png

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,136 +11,143 @@ import {
   FaGithub,
   FaLocationArrow,
   FaSteam,
+  FaTwitch,
   FaTwitter,
   FaUserGraduate,
   FaXbox,
   FaYoutube,
 } from 'react-icons/fa';
-
-import MyAvatar from '../../public/icons/nonick.png';
+import { TbBrandMinecraft } from 'react-icons/tb';
+import { SiMisskey } from 'react-icons/si';
+import MyAvatar from '../../public/icons/Karasu.png';
+import { Card } from '@/components/ui/card';
 
 export default function Home() {
   return (
     <main className='container space-y-6 py-6 lg:my-10'>
-      <FadeUpStagger>
-        <div className='flex h-16 items-center justify-end'>
-          <ThemeToggle />
-        </div>
-        <div className='grid grid-cols-12 grid-rows-3 gap-5 lg:gap-6'>
-          <FadeUpDiv className='col-span-12 row-span-3 space-y-5 md:space-y-7 lg:col-span-4 lg:space-y-8'>
-            <Image
-              className='pointer-events-none w-[150px] rounded-full shadow-xl lg:w-[200px]'
-              src={MyAvatar}
-              alt="nonick's avatar"
-            />
-            <section>
-              <h1 className='text-3xl font-black lg:text-4xl'>NoNICK</h1>
-              <h2 className='text-lg text-muted-foreground lg:text-xl'>
-                なんちゃってコンテンツクリエイター
-              </h2>
-            </section>
-            <div className='grid gap-1'>
-              <div className='flex items-center gap-2 text-muted-foreground'>
-                <FaBirthdayCake />
-                <p>200X年10月17日</p>
+      <div>
+        <FadeUpStagger>
+          <div className='flex h-16 items-center justify-end'>
+            <ThemeToggle />
+          </div>
+          <div className='grid grid-cols-12 grid-rows-3 gap-5 lg:gap-6'>
+            <FadeUpDiv className='col-span-12 row-span-3 space-y-5 md:space-y-7 lg:col-span-4 lg:space-y-8'>
+              <Image
+                className='pointer-events-none w-[150px] rounded-full shadow-xl lg:w-[200px]'
+                src={MyAvatar}
+                alt="Karasu's avatar"
+              />
+              <section>
+                <h1 className='text-3xl font-black lg:text-4xl'>カラス</h1>
+                <h2 className='text-lg text-muted-foreground lg:text-xl'>
+                  {`( ᐛ👐)`}
+                </h2>
+              </section>
+              <div className='grid gap-1'>
+                <div className='flex items-center gap-2 text-muted-foreground'>
+                  <FaBirthdayCake />
+                  <p>???</p>
+                </div>
+                <div className='flex items-center gap-2 text-muted-foreground'>
+                  <FaLocationArrow />
+                  <p>日本 / Japan</p>
+                </div>
+                <div className='flex items-center gap-2 text-muted-foreground'>
+                  <FaUserGraduate />
+                  <p>学生</p>
+                </div>
               </div>
-              <div className='flex items-center gap-2 text-muted-foreground'>
-                <FaLocationArrow />
-                <p>日本 / Japan</p>
-              </div>
-              <div className='flex items-center gap-2 text-muted-foreground'>
-                <FaUserGraduate />
-                <p>学生</p>
-              </div>
-            </div>
-          </FadeUpDiv>
-          <LinkCard
-            className='col-span-12 grid gap-3 p-6 transition-colors hover:border-youtube md:col-span-6 lg:col-span-4'
-            href='https://youtube.com/nonick_mc'
-          >
-            <CardIcon className='bg-youtube'>
-              <FaYoutube size={25} />
-            </CardIcon>
-            <CardName name='YouTube' id='@nonick_mc' />
-          </LinkCard>
-          <LinkCard
-            className='col-span-6 grid gap-3 p-6 transition-colors hover:border-twitter md:col-span-3 lg:col-span-2'
-            href='https://twitter.com/nonick_mc'
-          >
-            <CardIcon className='bg-twitter'>
-              <FaTwitter size={25} />
-            </CardIcon>
-            <CardName name='Twitter' id='@nonick_mc' />
-          </LinkCard>
-          <LinkCard
-            className='col-span-6 grid gap-3 p-6 transition-colors hover:border-discord md:col-span-3 lg:col-span-2'
-            href='https://discord.gg/nonick-mc'
-          >
-            <CardIcon className='bg-discord'>
-              <FaDiscord size={25} />
-            </CardIcon>
-            <CardName name='Discord' id='@nonick_mc'>
-              <DiscordPartnerIcon />
-            </CardName>
-          </LinkCard>
-          <FadeUpCard className='col-span-6 grid gap-3 p-6 transition-colors hover:border-xbox md:col-span-3 lg:col-span-2'>
-            <CardIcon className='bg-xbox'>
-              <FaXbox size={25} />
-            </CardIcon>
-            <CardName name='Xbox' id='NULL1017' />
-          </FadeUpCard>
-          <LinkCard
-            className='col-span-6 grid gap-3 p-6 transition-colors hover:border-steam md:col-span-3 lg:col-span-2'
-            href='https://steamcommunity.com/id/nonick-mc/'
-          >
-            <CardIcon className='bg-steam'>
-              <FaSteam size={25} />
-            </CardIcon>
-            <CardName name='Steam' id='NULL1017' />
-          </LinkCard>
-          <LinkCard
-            className='col-span-12 flex justify-between p-6 transition-colors hover:border-black dark:hover:border-gray-500 md:col-span-6 lg:col-span-4'
-            href='https://github.com/nonick-mc'
-          >
-            <div className='grid gap-3'>
-              <CardIcon className='bg-github'>
-                <FaGithub size={25} />
+            </FadeUpDiv>
+            <LinkCard
+              className='col-span-6 grid gap-3 p-6 transition-colors hover:border-twitter md:col-span-3 lg:col-span-2'
+              href='https://twitter.com/Columba_Karasu'
+            >
+              <CardIcon className='bg-twitter'>
+                <FaTwitter size={25} />
               </CardIcon>
-              <CardName name='GitHub' id='@nonick-mc' />
-            </div>
-            <div className='grid grid-cols-2 gap-3'>
-              <Image width={32} height={32} src='/programLangs/JavaScript.svg' alt='javascript' />
-              <Image width={32} height={32} src='/programLangs/TypeScript.svg' alt='typescript' />
-            </div>
-          </LinkCard>
-          <LinkCard
-            className='col-span-12 grid gap-3 p-6 transition-colors hover:border-black dark:hover:border-gray-500 md:col-span-6 lg:col-span-4'
-            href='https://nonick-js.com'
-          >
-            <Image
-              width={40}
-              height={40}
-              className='rounded-2xl'
-              src='/icons/nonickjs.png'
-              alt='nonick.js icon'
-            />
-            <CardName name='NoNICK.js' id='サーバー管理に役立つ多機能DiscordBot' />
-          </LinkCard>
-          <LinkCard
-            className='col-span-12 grid gap-3 p-6 transition-colors hover:border-black dark:hover:border-gray-500 md:col-span-6 lg:col-span-4'
-            href='https://github.com/nonick-mc/nonick.stats'
-          >
-            <Image
-              width={40}
-              height={40}
-              className='rounded-2xl'
-              src='/icons/nonickstats.png'
-              alt='nonick.stats icon'
-            />
-            <CardName name='NoNICK.stats' id='The HIVEサーバーの統計を表示するDiscordBOT' />
-          </LinkCard>
-        </div>
-      </FadeUpStagger>
+              <CardName name='Twitter' id='@Columba_Karasu' />
+            </LinkCard>
+            <LinkCard
+              className='col-span-6 grid gap-3 p-6 transition-colors hover:border-discord md:col-span-3 lg:col-span-2'
+              href='https://discord.gg/T3GPnmxXt6'
+            >
+              <CardIcon className='bg-discord'>
+                <FaDiscord size={25} />
+              </CardIcon>
+              <CardName name='Discord' id='@kara_su' />
+            </LinkCard>
+            <FadeUpCard className='col-span-6 grid gap-3 p-6 transition-colors hover:border-xbox md:col-span-3 lg:col-span-2'>
+              <CardIcon className='bg-xbox'>
+                <FaXbox size={25} />
+              </CardIcon>
+              <CardName name='Xbox' id='Karasu256' />
+            </FadeUpCard>
+            <LinkCard
+              className='col-span-6 grid gap-3 p-6 transition-colors hover:border-steam md:col-span-3 lg:col-span-2'
+              href='https://steamcommunity.com/profiles/76561199134933953/'
+            >
+              <CardIcon className='bg-steam'>
+                <FaSteam size={25} />
+              </CardIcon>
+              <CardName name='Steam' id='KPW-Karasu' />
+            </LinkCard>
+            <LinkCard
+              className='col-span-6 grid gap-3 p-6 transition-colors hover:border-purple-500 md:col-span-3 lg:col-span-2'
+              href='https://www.twitch.tv/hashibutogarasu/'
+            >
+              <CardIcon className='bg-purple-500'>
+                <FaTwitch size={25} />
+              </CardIcon>
+              <CardName name='Twitch' id='hashibutogarasu' />
+            </LinkCard>
+            <LinkCard
+              className='col-span-12 flex justify-between p-6 transition-colors hover:border-black dark:hover:border-gray-500 md:col-span-6 lg:col-span-4'
+              href='https://github.com/Hashibutogarasu'
+            >
+              <div className='grid gap-3'>
+                <CardIcon className='bg-github'>
+                  <FaGithub size={25} />
+                </CardIcon>
+                <CardName name='GitHub' id='@Hashibutogarasu' />
+              </div>
+              <div className='grid grid-cols-3 gap-3'>
+                <Image width={32} height={32} src='/programLangs/JavaScript.svg' alt='javascript' />
+                <Image width={32} height={32} src='/programLangs/TypeScript.svg' alt='typescript' />
+                <Image width={32} height={32} style={{ backgroundColor: "white", borderRadius: "5px" }} src='/programLangs/java-icon.svg' alt='java' />
+              </div>
+            </LinkCard>
+            <FadeUpCard
+              className='col-span-6 grid gap-3 p-6 transition-colors hover:border-green-600 md:col-span-3 lg:col-span-2'>
+              <CardIcon>
+                <Image width={32} height={32} src='/icons/Minecraft_Launcher_MS_Icon.png' alt='Minecraft Java Edition' />
+              </CardIcon>
+              <CardName name='Minecraft Java Edition' id='kara_su' />
+            </FadeUpCard>
+            <LinkCard
+              className='col-span-6 grid gap-3 p-6 transition-colors hover:border-green-500 md:col-span-3 lg:col-span-2' href='https://misskey.io/@_Karasu'>
+              <CardIcon className='bg-green-500'>
+                <SiMisskey size={25} />
+              </CardIcon>
+              <CardName name='misskey.io' id='@_Karasu' />
+            </LinkCard>
+            <FadeUpCard
+              className='col-span-6 grid gap-3 p-6 transition-colors hover:border-white md:col-span-3 lg:col-span-2'>
+              <CardIcon>
+                <Image width={32} height={32} src='/icons/genshin_impact.png' alt='Genshin Impact' />
+              </CardIcon>
+              <CardName name='Genshin Impact' id='806193489 Rank:59' />
+            </FadeUpCard>
+            <FadeUpCard
+              className='col-span-6 grid gap-3 p-6 transition-colors hover:border-white md:col-span-3 lg:col-span-2'>
+              <CardIcon>
+                <Image width={32} height={32} src='/icons/starrail.png' alt='Honkai Starrail' />
+              </CardIcon>
+              <CardName name='Honkai Starrail' id='800498009 Rank:68' />
+            </FadeUpCard>
+          </div>
+        </FadeUpStagger>
+      </div>
+
     </main>
   );
 }
--- src/config/site.ts
+++ src/config/site.ts
@@ -2,11 +2,11 @@ import { SiteConfig } from '@/types';
 
 export const siteConfig: SiteConfig = {
   metadata: {
-    name: 'NoNICK',
-    description: 'なんちゃってコンテンツクリエイター',
-    twitter_id: '@nonick_mc',
+    name: 'Karasu',
+    description: '( ᐛ👐)',
+    twitter_id: '@Columba_Karasu',
     url: new URL(
-      process.env.NEXT_PUBLIC_VERCEL_URL || `http://localhost:${process.env.PORT || '3000'}`,
+      process.env.NEXT_PUBLIC_VERCEL_URL || `http://localhost:${process.env.PORT || '8080'}`,
     ),
   },
 };
