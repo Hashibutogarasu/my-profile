7abe163a1f9eb9e27bd952251048144415e65051
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2023-10-15 19:47:09 +0900

    Changes to be committed: 	modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     assetPrefix: isProd ? "/my-profile" : "",
     basePath: isProd ? "/my-profile" : "",
-    unoptimized: true,
 };
 
 module.exports = nextConfig;
