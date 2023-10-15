79ab4e35d275798e51b389f327ffa1fa70c39156
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2023-10-15 19:19:03 +0900

    modified:   .github/workflows/nextjs.yml modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: process.env.NODE_ENV === "production" ? "/my-profile" : ""
+    assetPrefix: process.env.NODE_ENV === "production" ? "/my-profile" : "",
+    images:[
+        "/icons/genshin_impact.png",
+        "/icons/starrail.png"
+    ]
 };
 
 module.exports = nextConfig;
