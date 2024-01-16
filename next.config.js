17abe028bf712c71a237d43551e1622a6fba498b
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2024-01-16 14:48:33 +0900

    Update next.config.js with assetPrefix and basePath

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 const isProd = process.env.NODE_ENV === 'production';
+const urlPrefix = '/my-profile';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = isProd ? {
-    basePath: process.env.GITHUB_ACTIONS ? "/githubpages.karasu256.com" : "",
+    assetPrefix: urlPrefix,
+    basePath: urlPrefix,
     output: 'export',
 } : {
     unoptimized: true,
