43a88637a1d178b6b54af6d6bc1b0e6af3ca9ac0
Hashibutogarasu <corvusmacrorhynchosjaponensis@gmail.com>
2024-01-16 15:00:58 +0900

    Remove unused code in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
-const urlPrefix = '/';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = isProd ? {
-    assetPrefix: urlPrefix,
-    basePath: urlPrefix,
     output: 'export',
 } : {
     unoptimized: true,
